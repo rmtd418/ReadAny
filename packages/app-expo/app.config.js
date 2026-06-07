8546d638084371612a109827de6a4a86275320aa
codedogQBY <1369175442@qq.com>
2026-06-07 22:31:48 +0800

    chore(release): bump version to 1.3.3

--- packages/app-expo/app.config.js
+++ packages/app-expo/app.config.js
@@ -6,7 +6,7 @@ module.exports = {
   expo: {
     name: variant.name,
     slug: "readany",
-    version: "1.3.2",
+    version: "1.3.3",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "automatic",
